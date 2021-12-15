4bcd15bc6a267a03a0900e337a72e4f81946926a
PhucNood <phucnvhd772000@gmail.com>
2021-12-15 09:43:08 +0700

    final update full hd khong che

--- J1.S.P0052/src/j1/s/p0052/J1SP0052.java
+++ J1.S.P0052/src/j1/s/p0052/J1SP0052.java
@@ -17,31 +17,7 @@ public class J1SP0052 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArrayList<EastAsianCountry> list = new ArrayList<>();
-        ManageEastAsiaCountries manager = new ManageEastAsiaCountries(list);
-        //loop until user want to exist
-
-        while (true) {
-            manager.showMenu();
-            int choice = manager.getInputChoice();
-            switch (choice) {
-                case 1:
-                    manager.inputCountry();
-                    break;
-                case 2:
-                    manager.Display();
-                    break;
-                case 3:
-                    manager.Search();
-                    break;
-                case 4:
-                    manager.Sort();
-                    break;
-                case 5:
-                    return;
-
-            }
-        }
+        new ManageEastAsiaCountries();
 
     }
 
--- J1.S.P0052/src/j1/s/p0052/ManageEastAsiaCountries.java
+++ J1.S.P0052/src/j1/s/p0052/ManageEastAsiaCountries.java
@@ -18,152 +18,189 @@ import java.util.Scanner;
 public class ManageEastAsiaCountries {
 
     private final Scanner sc = new Scanner(System.in);
+    private ArrayList<EastAsianCountry> list;
 
-    ArrayList<EastAsianCountry> list;
-
-    public ManageEastAsiaCountries(ArrayList<EastAsianCountry> list) {
-        this.list = list;
-    }
-
-    public void showMenu() {
-        System.out.println("                                MENU                                     ");
-        System.out.println("========================================================================");
-        System.out.println("1. Input the information of 11 countries in East Asia");
-        System.out.println("2. Display the information of country you've just input");
-        System.out.println("3. Search the information of country by user-entered name");
-        System.out.println("4. Display the information of countries sorted name in ascending");
-        System.out.println("5. Exit");
-        System.out.println("========================================================================");
+    public ManageEastAsiaCountries() {
+        list = new ArrayList<>();
+        program();
 
     }
 
-    // get value of your choice exactly format  
-    public int getInputChoice() {
-        System.out.print("Enter your choice: ");
-        //  run the loop untill user enter right input number of choice
+    private void program() {
         while (true) {
-            // check exception input for number
-            try {
-                int number = Integer.parseInt(sc.nextLine());
-                // check if user enter number outer range
-                if ((number <= 5) && (number >= 1)) {
-                    return number;
-                } else {
-                    System.err.println("out of range 1 to 5");
-                    System.out.print("Please enter again: ");
-                }
-            } catch (NumberFormatException e) { // check if number is not format
-                System.err.println("Must be a Ingeger number");
-                System.out.print("Please enter again: ");
-            }
-        }
-    }
-
-    // get value of Float number exactly format  
-    public float getInputFloat(float min, float max, String mess) {
-        System.out.print(mess);
-        float result;
-        while (true) {
-            // check exception input for number
-            try {
-                result = Float.parseFloat(sc.nextLine().trim());
-                if (result >= 0 && result <= Float.MAX_VALUE) {
+            int choice = showMenuAndChoice(); // show menu and choice
+            switch (choice) {
+                case 1:
+                    addContry();
                     break;
-                } else {
-                    System.err.print("please enter over 0 ");
+                case 2:
+                    displayList();
+                    break;
+                case 3:
+                    searchByName();
+                    break;
+                case 4:SortAcendingbyName();
+                    break;
+                case 5:
+                    return;
+            }
+        }
+    }
+
+    private int showMenuAndChoice() {  // show menu and return choice
+        System.out.println("                            MENU\n"
+                + // show menu
+                "==========================================================================\n"
+                + "1. Input the information of 11 countries in East Asia\n"
+                + "2. Display the information of country you've just input\n"
+                + "3. Search the information of country by user-entered name\n"
+                + "4. Display the information of countries sorted name in ascending order  \n"
+                + "5. Exit \n"
+                + "==========================================================================\n"
+        );
+        int choice = checkInt("Enter your choice: ", 1, 5);
+        return choice;
+    }
+
+    private int checkInt(String mess, int min, int max) { //check data type int in this case is choice
+        int number;
+        while (true) {// loop until  true/return
+            System.out.print(mess); // show the message/ title you want
+            try {
+                number = Integer.parseInt(sc.nextLine().trim());
+                if (number >= min && number <= max) { // if in valid rage 
+                    return number;
+                } else {  // out of valid rage / in-valid rage
+                    System.err.println("Number must in rage " + min + " to " + max); // show warning error
                 }
-            } catch (NumberFormatException e) { // check if number is not format
-                System.err.println("Must be a Positive number");
-                System.out.print("Enter agian: ");
+            } catch (NumberFormatException e) {
+                System.err.println("Must is number format!Please! "); // show warning exception
             }
-
         }
-        return result;
+
     }
 
-    //get string from input of user
-    public String getInputString(String mess) {
-        System.out.print(mess);
-        //loop until user input correct
-        while (true) {
-            String result = sc.nextLine().trim();
-            if (result.isEmpty()) {
-                System.err.println("Not empty");
-                System.out.print("Enter again: ");
+    private float checkFloat(String mess, float min, float max) { //check data type float in this case is area data
+        float number;
+        while (true) {// loop until  true/return
+            System.out.println(mess); // show the message/ title you want
+            try {
+                number = Float.parseFloat(sc.nextLine().trim());
+                if (number > min && number <= max) { // if in valid rage 
+                    return number;
+                } else {  // out of valid rage / in-valid rage
+                    System.err.println("Number must in rage " + min + " to " + max);
+                }
+            } catch (NumberFormatException e) {
+                System.err.println("Must is number format!Please! ");// show warning exception
+            }
+        }
+
+    }
+
+    private String checkString(String mess) { //check data type String
+        String s;
+        //  System.out.println(mess); 
+        while (true) { // loop until  true/return
+            // show the message/ title you want
+            System.out.println(mess);
+            s = sc.nextLine().trim();
+            if (s.isEmpty()) {  // conditon check String empty or not
+                System.out.println("Your input is Empty! "); // show warning error
+
             } else {
-                return result;
+                return s;
             }
         }
+
     }
 
-    //check country exist by code
-    public EastAsianCountry CheckCode(String code) {
-        for (EastAsianCountry eastAsianCountry : list) {
-            if (code.equalsIgnoreCase(eastAsianCountry.getCountryCode())) {
-                return eastAsianCountry;
+    private EastAsianCountry checkContryExist(String code) { // code is super-key of EAC
+        for (EastAsianCountry eac : list) {
+            if (eac.getCountryCode().equalsIgnoreCase(code)) { // if key is exist
+                return eac; // exist that country
             }
+
         }
         return null;
     }
 
-    public void inputCountry() {
-        String code = getInputString("Country code: ");
-        while (CheckCode(code) != null) {
-            System.err.println("Country exist!");
-            code = getInputString("Enter country code again: ");
+    private void addContry() {
+        String code = checkString("Enter code of country: "); // check string input
+        while (checkContryExist(code) != null) { // loop while contry existed
+            System.err.println("Contry existed!");
+            code = checkString("Enter code of country: "); // re-input code
         }
-        String name = getInputString("Country name: ");
-        float totalArea = getInputFloat(0, Float.POSITIVE_INFINITY, "Total Area: ");
-        String terrain = getInputString("Terrain: ");
-        list.add(new EastAsianCountry(code, name, totalArea, terrain));
-        System.out.println("Add success");
+        String name = checkString("Enter name of country: ");
+        float area = checkFloat("Enter total Area: ", 0, Float.POSITIVE_INFINITY); // check float input rage greater 0 to positive infinive
+        String terain = checkString("Enter terrain of country: "); // check string input
+        list.add(new EastAsianCountry(code, name, area, terain)); // add new country you input to list
     }
 
-    public void Display() {
+    private void displayList() { // display list
         if (list.isEmpty()) {
-            System.err.println("List still empty!!!");
+            System.err.println("List is still empty!"); //if list is empty alert
             System.out.println("");
         } else {
-            System.out.printf("%-10s%-25s%-20s%-25s\n", "Code", "Name", "Total Area", "Terrain");
-            for (EastAsianCountry eastAsianCountry : list) {
-                eastAsianCountry.display();
+            System.out.printf("%-10s%-25s%-20s%-25s\n", "ID", "Name", "Total Area",
+                    "Terrain"); // print title
+            for (EastAsianCountry eac : list) { // loop to show each eac
+                eac.display(); // display infor of each
             }
             System.out.println("");
         }
 
     }
 
-    public void Search() {
-
-        String name = getInputString("Enter country name: ");
-        boolean inlist = false;
-        for (EastAsianCountry eastAsianCountry : list) {
-            if (name.equalsIgnoreCase(eastAsianCountry.getCountryName())) {
-                System.out.printf("%-10s%-25s%-20s%-25s\n", "Code", "Name", "Total Area", "Terrain");
-                eastAsianCountry.display();
-                inlist = true;
-                break;
-            }
-
-        }
-        if (inlist == false) {
-            System.err.println("Country not in list");
-        }
-    }
-
-    public void Sort() {
+    private void searchByName() {
         if (list.isEmpty()) {
-            System.err.println("List still empty!!!");
             System.out.println("");
+            System.err.println("List is still empty!"); //if list is empty alert
+
         } else {
-            list.sort((c1,c2)->c1.countryName.compareToIgnoreCase(c2.countryName));
-            System.out.printf("%-10s%-25s%-20s%-25s\n", "Code", "Name", "Total Area", "Terrain");
-            for (EastAsianCountry eastAsianCountry : list) {
-                eastAsianCountry.display();
+            String searchName = checkString("Enter the name you want to search for: "); // input name coutry you want to search
+            boolean isFound = false;
+            for (EastAsianCountry eac : list) {
+                if (searchName.equalsIgnoreCase(eac.getCountryName())) {
+                    isFound = true; // founded
+                    System.out.printf("%-10s%-25s%-20s%-25s\n", "ID", "Name", "Total Area",
+                            "Terrain"); // print title
+                    eac.display();
+                    break; // out of loop
+                } else {
+                    isFound = false; // not founded
+                }
+
+            }
+            if (isFound == false) {
+                System.err.println("Not found this country!");
+                System.out.println("");
+            }
+        }
+    }
+    
+    
+    private void SortAcendingbyName(){ // sort list acending by name
+        ArrayList<EastAsianCountry> sortList = new ArrayList<>(list); // create new list same elemets  to not efected to origin list
+        if (sortList.isEmpty()) {
+            System.out.println("");
+            System.err.println("List is still empty!"); //if list is empty alert
+
+        } else {
+            
+            // sort list
+            sortList.sort((c1,c2)->c1.getCountryName().compareToIgnoreCase(c2.getCountryName())); /*use sort build-in funtion of Collect this have 
+                                                                                                       parameter is a funtional interface Comparator so can use 
+                                                                                                     lamda expression to overide method compare of this interface
+                                                                                                        */ 
+            // display sort list
+            System.out.printf("%-10s%-25s%-20s%-25s\n", "ID", "Name", "Total Area",
+                            "Terrain"); // print titled
+            for (EastAsianCountry eac: sortList) {
+                eac.display();
             }
             System.out.println("");
         }
-      
     }
 
 }
